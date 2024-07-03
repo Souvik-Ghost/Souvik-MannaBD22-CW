6caa689c0143370d01d6a66e536ac21e96c4f74c
Ghost <103312649+Souvik-Ghost@users.noreply.github.com>
2024-07-03 16:51:11 +0000

    feat: index js and vercel files modified

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ app.listen(port, () => {
   console.log(`The server listening at http://localhost:${port}`);
 });
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let ages = [10, 20, 30, 15, 17, 25];
-let words = ["apple", "banana", "cherry", "date", "fig", "grape"];
-let fileSizes = [50, 200, 75, 120, 30, 90, 150];
-
 //1
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function filterEvenNumbers(ele) {
   return ele % 2 === 0;
 }
@@ -21,6 +17,7 @@ app.get("/even-numbers", (req, res) => {
 //even-numbers
 
 //2
+let ages = [10, 20, 30, 15, 17, 25];
 function filterAges(ele) {
   return ele > 18;
 }
@@ -31,6 +28,7 @@ app.get("/adult-ages", (req, res) => {
 //adult-ages
 
 //3
+let words = ["apple", "banana", "cherry", "date", "fig", "grape"];
 function filterLongWords(ele, filterParam) {
   return ele.length > filterParam;
 }
@@ -42,6 +40,7 @@ app.get("/long-words", (req, res) => {
 //long-words?filterParam=5//req.query.filterParam
 
 //4
+let fileSizes = [50, 200, 75, 120, 30, 90, 150];
 function filterSmallFiles(ele, filterParam) {
   return ele < filterParam;
 }
